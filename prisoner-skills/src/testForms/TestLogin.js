021f837fafa6666d895cd55abee816ae40052cea
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-03 11:23:36 -0500

    created testForms directory and used it to test the end points from the backend -- added formik and yup

--- prisoner-skills/src/testForms/TestLogin.js
+++ prisoner-skills/src/testForms/TestLogin.js
@@ -1,68 +1,54 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import {Formik, Form, Field} from 'formik';
+import * as Yup from 'yup';
 import { Link } from 'react-router-dom';
-import Nav from './universal/Nav';
-import Footer from './universal/Footer';
+import Nav from '../components/universal/Nav'
+import Footer from '../components/universal/Footer';
 
 const TestLogin = () => {
-  // const initialState = {
-  //   username: '',
-  //   password: ''
-  // }
-
-  // const [credentials, setCredentials] = useState(initialState);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleUsername = e => {
-    setUsername(e.target.value);
-    console.log('username: ', username);
-  }
-
-  const handlePassword = e => {
-    setPassword(e.target.value);
-    console.log('password: ', password);
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault();
-
-    axios.post(`https://prisoner-skills-backend.herokuapp.com/api/auth/login`, username, password)
-      .then(res => {
-        // localStorage.setItem('TOKEN', res.data.token);
-        // this.props.history.push('/profile')
-        console.log(res)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-
-    setUsername('');
-    setPassword('');
-  }
-
   return (
     <div>
       <Nav />
       <h3>Login Form</h3>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="username"
-          value={username}
-          onChange={handleUsername}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          value={password}
-          onChange={handlePassword}
-        />
 
-        <button>Log In</button>
-      </form>
+      <Formik
+        initialValues={{
+          username: "",
+          password: ""
+        }}
+        onSubmit={(values, tools) => {
+          tools.resetForm();
+          console.log(values);
+          axios.post(`https://prisoner-skills-backend.herokuapp.com/api/auth/login`, values)
+            .then(res => {
+              // localStorage.setItem('TOKEN', res.data.token);
+              // this.props.history.push('/profile')
+              console.log(res)
+            })
+            .catch(err => {
+              console.log(err)
+            })
+        }}
+        render={props => {
+          return (
+            <Form>
+              <Field
+                type="text"
+                name="username"
+                placeholder="username"
+              />
+              <Field
+                type="password"
+                name="password"
+                placeholder="password"
+              />
+              <input type="submit" />
+            </Form>
+          )
+        }}
+      />
+
       <span>Don't have an account yet?</span> <span><Link to="/signup">Sign Up Here</Link></span>
       <Footer />
     </div>
@@ -70,6 +56,7 @@ const TestLogin = () => {
 };
 
 export default TestLogin;
+
 
 /*
 class TestLogin extends React.Component {
