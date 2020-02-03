830610a165aa26373534cb3b8547a6e1a82ea327
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-03 13:42:46 -0500

    added a data.js file and filled it with mock prison data

---
+++ prisoner-skills/src/data.js
@@ -0,0 +1,122 @@
+export const data = [
+  {
+    "prison_name": "Prison One",
+    "number_of_prisoners": 69,
+    "prison_address": "123",
+    "inmates": [
+      {
+        name: 'Smeagol',
+        skills: ['sewing', 'cooking', 'wood working'],
+        availability: false
+      },
+    ]
+  },
+  {
+    "prison_name": "Prison Two",
+    "number_of_prisoners": 45,
+    "prison_address": "456",
+    "inmates": [
+      {
+        name: 'Galadriel',
+        skills: ['carpentry', 'magic'],
+        availability: false
+      },
+    ]
+  },
+  {
+    "prison_name": "Prison Three",
+    "number_of_prisoners": 85,
+    "prison_address": "789",
+    "inmates": [
+      {
+        name: 'Arwen',
+        skills: ['plumbing', 'cooking', 'shoe maker'],
+        availability: false
+      },
+    ]
+  },
+  {
+    "prison_name": "Prison Four",
+    "number_of_prisoners": 40,
+    "prison_address": "321",
+    "inmates": [
+      {
+        name: 'Boromir',
+        skills: ['foraging', 'mathematics', 'sculpting'],
+        availability: false
+      },
+    ]
+  },
+  {
+    "prison_name": "Prison Five",
+    "number_of_prisoners": 20,
+    "prison_address": "654",
+    "inmates": [
+      {
+        name: 'Gimli',
+        skills: ['music', 'janitor'],
+        availability: false
+      },
+    ]
+  },
+  {
+    "prison_name": "Prison Six",
+    "number_of_prisoners": 88,
+    "prison_address": "987",
+    "inmates": [
+      {
+        name: 'Legalos',
+        skills: ['swimming', 'archery'],
+        availability: false
+      },
+    ]
+  },
+  {
+    "prison_name": "Prison Seven",
+    "number_of_prisoners": 56,
+    "prison_address": "741",
+    "inmates": [
+      {
+        name: 'Aragorn',
+        skills: ['sword fighting', 'cooking', 'boxing'],
+        availability: false
+      },
+    ]
+  },
+  {
+    "prison_name": "Prison Eight",
+    "number_of_prisoners": 99,
+    "prison_address": "852",
+    "inmates": [
+      {
+        name: 'Gandalf',
+        skills: ['communication', 'multiple languages', 'horse riding'],
+        availability: false
+      },
+    ]
+  },
+  {
+    "prison_name": "Prison Nine",
+    "number_of_prisoners": 12,
+    "prison_address": "963",
+    "inmates": [
+      {
+        name: 'Frodo',
+        skills: ['long distance walking', 'diplomatic'],
+        availability: false
+      },
+    ]
+  },
+  {
+    "prison_name": "Prison Ten",
+    "number_of_prisoners": 77,
+    "prison_address": "753",
+    "inmates": [
+      {
+        name: 'Sam',
+        skills: ['eating', 'cooking'],
+        availability: false
+      },
+    ]
+  }
+]
